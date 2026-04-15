89e88dfdb55a555713299670ca4d1ee3c657a1b9
suvigna <chinnam.suvigna@gmail.com>
2026-04-15 09:10:16 +0530

    button

---
+++ Button/index.js
@@ -0,0 +1,9 @@
+//events
+
+const myFun = () =>
+{
+    //console.log("see u");
+    let display=document.getElementById("display");
+    //display.innerHTML="bye";
+    display.innerHTML= display.innerHTML ==="Hello...."? "bye" :"Hello...."
+};
