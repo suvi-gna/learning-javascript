16a5ae4ea1d45c6af3d037f4c1a566e811db7b8d
suvigna <chinnam.suvigna@gmail.com>
2026-04-15 09:48:04 +0530

    buttonimage

--- Button/index.js
+++ Button/index.js
@@ -5,5 +5,15 @@ const myFun = () =>
     //console.log("see u");
     let display=document.getElementById("display");
     //display.innerHTML="bye";
-    display.innerHTML= display.innerHTML ==="Hello...."? "bye" :"Hello...."
+    
+    display.innerHTML= display.innerHTML === "Hello...."? "bye" :"Hello...."
+    if(display.innerHTML == "Hello....")
+    {
+            display.style.color="green";
+    }
+    else
+    {
+        display.style.color="red";
+    }
+    
 };
---
+++ buttonimage/index.js
@@ -0,0 +1,16 @@
+
+let isShow=true;
+const showImage = () =>
+{
+    let display=document.getElementById("display");
+    if(isShow)
+    {
+        display.src="https://images.unsplash.com/photo-1530092285049-1c42085fd395?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3VtbWVyJTIwZmxvd2Vyc3xlbnwwfHwwfHx8MA%3D%3D";
+        isShow=!isShow;
+    }
+    else
+    {
+        display.src="https://media1.tenor.com/m/Io0g8LOf8nMAAAAC/dog-awkward.gif";
+        isShow=!isShow;
+    }
+};
