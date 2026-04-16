7016439d742980f782da6c8188b6feca8a360975
suvigna <chinnam.suvigna@gmail.com>
2026-04-16 09:47:06 +0530

    fgh

--- buttonimage/index.js
+++ buttonimage/index.js
@@ -6,11 +6,11 @@ const showImage = () =>
     if(isShow)
     {
         display.src="https://images.unsplash.com/photo-1530092285049-1c42085fd395?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3VtbWVyJTIwZmxvd2Vyc3xlbnwwfHwwfHx8MA%3D%3D";
-        isShow=!isShow;
+        isShow=false;
     }
     else
     {
         display.src="https://media1.tenor.com/m/Io0g8LOf8nMAAAAC/dog-awkward.gif";
-        isShow=!isShow;
+        isShow=true;
     }
 };
