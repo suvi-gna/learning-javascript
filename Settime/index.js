72548fea2a88c1a0a8780842e8509788ce4762f5
suvigna <chinnam.suvigna@gmail.com>
2026-04-22 08:56:22 +0530

    settime

---
+++ Settime/index.js
@@ -0,0 +1,19 @@
+
+
+//try catch
+try{
+
+}
+catch(err)
+{
+
+}
+//call backs
+
+function myfun()
+{
+    console.log("hi....");
+    
+}
+setTimeout(myfun,200)   //it run only one setTimeout
+//setInterval(myfun,2000)   //it runs infinity times
--- formpage/index.js
+++ formpage/index.js
@@ -12,19 +12,27 @@ const validateForm= (event)=>
     if(username === "" && userpass === "")
     {
         msg.innerText="please enter username & userpass"
+        username.style.bordercolor="red";
+        userpass.style.bordercolor="red"
     }
     else if(username==="")
     {
     
         msg.innerText="plese enter your name"
+        username.style.bordercolor="red"
+        userpass.style.bordercolor="red"
     }
     else if(userpass === "" )
     {
         msg.innerText="please enter your password"
+        username.style.bordercolor="red"
+        userpass.style.bordercolor="red"
     }
     else if(username !== "" && userpass !== "")
     {
                 msg.innerText="succ"
+                username.style.bordercolor="blue"
+                userpass.style.bordercolor="blue"
     }
 
 }
